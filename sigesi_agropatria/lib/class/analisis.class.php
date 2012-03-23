52d2b13cd54b6e4d20b950bb392c1419dde0fe4c
tabordajuan <tabordajuan@79bc816c-a3bf-c122-f00e-4cf5b6f85184>
2012-03-23 16:16:30 +0000

    Se cambio la clase Analisis para permitir hacer busqueda por codigo, id tanto un elemento como una lista de cada uno

--- sigesi_agropatria/lib/class/analisis.class.php
+++ sigesi_agropatria/lib/class/analisis.class.php
@@ -17,12 +17,16 @@ class Analisis extends Model {
         return $this->id = $id;
     }
     
-    function listadoResultados($id_recepcion, $id_analisis=null) {
-        $query = "SELECT id_recepcion, id_analisis, muestra1, muestra2, muestra3
-                    FROM si_analisis_resultado WHERE '1' ";
-        $query .= (!empty($id_recepcion)) ? " AND id_recepcion = '$id_recepcion'" : "";
-        $query .= (!empty($id_analisis)) ? " AND id_analisis = '$id_analisis'" : "";
-        $query .= "ORDER BY id_recepcion";
+    function listadoResultados($id_recepcion=null, $id_despacho=null, $id_analisis=null, $codigo=null) {
+        $query = "SELECT a.codigo, a.tipo_analisis, ar.id_recepcion, ar.id_analisis, ar.muestra1, ar.muestra2, ar.muestra3
+            FROM si_analisis_resultado ar 
+            INNER JOIN si_analisis a ON (ar.id_analisis=a.id) WHERE '1' ";
+        $query .= (!empty($id_recepcion)) ? " AND ar.id_recepcion = '$id_recepcion'" : "";
+        $query .= (!empty($id_despacho)) ? " AND ar.id_despacho = '$id_despacho'" : "";
+        $query .= (!empty($id_analisis)) ? " AND a.id IN ($id_analisis)" : "";
+        $query .= (!empty($codigo)) ? " AND a.codigo IN ($codigo)" : "";
+        $query .= "ORDER BY ar.id_analisis";
+        
         $id = $this->_SQL_tool('SELECT', __METHOD__, $query);
         return $this->id = $id;
     }
@@ -35,10 +39,11 @@ class Analisis extends Model {
         $query .= (!empty($codigo)) ? " AND codigo = '$codigo'" : "";
         $query .= (!empty($estatus)) ? " AND estatus = '$estatus'" : "";
         $query .= (!empty($formula)) ? " AND formula = '$formula'" : "";
+        $query .= "ORDER BY codigo";
         return $this->_SQL_tool('SELECT', __METHOD__, $query);
     }
     
-    function buscarAC($id=null, $IdCultivo=null, $idCA=null) { 
+    function buscarAC($id=null, $IdCultivo=null, $idCA=null, $laboratorio=null) { 
     $query = "SELECT a.id, ac.id_cultivo, a.codigo, a.nombre, a.tipo_analisis,
                 ac.min_rec, ac.max_rec , ac.estatus 
                 FROM si_analisis_cultivo ac 
@@ -47,6 +52,8 @@ class Analisis extends Model {
     $query .= (!empty($id)) ? " AND a.id = '$Id'" : "";        
     $query .= (!empty($IdCultivo)) ? " AND ac.id_cultivo = '$IdCultivo'" : "";
     $query .= (!empty($IdCA)) ? " AND ac.id_centro_acopio = '$IdCA'" : "";
+    $query .= (!empty($IdCA)) ? " AND ac.id_centro_acopio = '$IdCA'" : "";
+    $query .= (!empty($laboratorio)) ? " AND ac.laboratorio IN ($laboratorio)" : "";    
     $query .= " ORDER BY cast(a.codigo as int)";
     return $this->_SQL_tool($this->SELECT, __METHOD__, $query); 
     }
