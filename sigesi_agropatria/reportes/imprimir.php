045f425d00d2c5153d2d8e045d5bfc333f192ce7
jedroviana <jedroviana@79bc816c-a3bf-c122-f00e-4cf5b6f85184>
2012-04-11 13:10:10 +0000

    Identacion en el archivo

--- sigesi_agropatria/reportes/imprimir.php
+++ sigesi_agropatria/reportes/imprimir.php
@@ -9,22 +9,27 @@
         case 'boleta_rechazo':
             $url = DOMAIN_ROOT."reportes/imprimir_boleta_rechazo.php?id=". $GPC['id']."&es_rechazado=".$GPC['es_rechazado'];
         break;
-    }    
+    }
+    
     $imprimir = file_get_contents($url);
     $mpdf=new mPDF();  
     $stylesheet = file_get_contents(DOMAIN_ROOT.'css/reportes.css');
     $mpdf->WriteHTML($stylesheet,1);
     $mpdf->WriteHTML($imprimir);
     $archivo=$GPC['reporte'].time().'.pdf';
-    $mpdf->Output($archivo,'D');
+    $mpdf->Output($archivo,'F');
+    
     /* SENTENCIA PARA IMPRESION INDICANDO LA IMPRESORA A TRAVES DEL PARAMETRO -P DEL LPR
      * $parametros="-P HP-LaserJet-p1606dn";
      * exec("lpr $parametros $archivo");
      */
+    
     exec("lpr $archivo");
-    exec("rm $archivo");    
+    exec("rm $archivo");
+    
     if(!empty($GPC['redir'])){
         $GPC['redir'] = (!empty($_SESSION['s_mov']) && !empty($_SESSION['s_lab'])) ? $GPC['redir'].'?mov='.$_SESSION['s_mov'].'&lab='.$_SESSION['s_lab'] : $GPC['redir'];
         header('location: '.DOMAIN_ROOT.'admin/'.$GPC['redir'].'.php');
     }
+
 ?>
